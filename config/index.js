db23c12a34b95aadd4427207e4f160d63ed097a2
huyongyong1992 <huyongyong1992@gmail.com>
2017-07-18 14:13:15 +0800

    init

--- config/index.js
+++ config/index.js
@@ -27,11 +27,6 @@ module.exports = {
         context: [ //代理路径
 
         ],
-        // CSS Sourcemaps off by default because relative paths are "buggy"
-        // with this option, according to the CSS-Loader README
-        // (https://github.com/webpack/css-loader#sourcemaps)
-        // In our experience, they generally work as expected,
-        // just be aware of this issue when enabling this option.
         cssSourceMap: false
     }
 }
