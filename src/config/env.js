8ad68220d56e3e6e195cac9c8b38e97ae7f7397c
Young <huyongyong1992@gmail.com>
2017-12-20 10:13:47 +0800

    配置项目环境

--- src/config/env.js
+++ src/config/env.js
@@ -1,22 +1,31 @@
 /**
  * 配置编译环境和线上环境之间的切换
- * 
- * baseUrl: 域名地址
+ *
+ * url: 接口域名地址
  * routerMode: 路由模式
+ * wxapiUrl： 微信授权需要的域名
+ * signUrl: 电子签名页面所在的域名
  * 
  */
-let baseUrl; 
+let url;
+let signUrl ;
 let routerMode;
-
-if (process.env.NODE_ENV == 'development') {
-	baseUrl = '';
-	routerMode = 'hash'
+let wxapiUrl;
+if (process.env.NODE_ENV === 'production') {
+	url = 'https://xxx.com';	//生产环境接口url
+  wxapiUrl = 'https://xxx/';
+	routerMode = 'hash';
+	signUrl = 'https://xxx.com';
 }else{
-	baseUrl = 'https://mainsite-restapi.ele.me';	//生产环境接口baseUrl
-	routerMode = 'hash'
+	url = 'http://xxx/api';
+	routerMode = 'hash';
+	signUrl = 'http://xxx.com';
+	wxapiUrl = 'http://xxx/api';
 }
 
 export {
-	baseUrl,
+	url,
 	routerMode,
+	signUrl,
+  wxapiUrl
 }
